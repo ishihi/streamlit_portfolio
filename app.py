4212d556af1a1ef261acdd8c0aec586d4b1f069e
ishihi <cxhmt443@yahoo.co.jp>
2024-11-12 16:19:36 +0900

    Add text elements to the app

--- app.py
+++ app.py
@@ -2,7 +2,29 @@ import streamlit as st
 
 # タイトルの設定
 
-st.title('私のStreamlitアプリ')
+st.title('StreamlitによるApp')
+st.header('レッスン3：テキスト要素の追加')
 
 # テキストの表示
-st.write('こんにちは、Streamlit！')
+st.write('こんにちはwawa、Streamlit！')
+# 通常のテキスト
+st.text('これは通常のテキストです。')
+# Markdown形式のテキスト
+st.markdown('これは **太字** で、 *イタリック* です。')
+# LaTeX形式の数式
+st.latex(r'\sqrt{x^2 + y^2} = z')
+
+# 情報メッセージ（青色）
+st.info('データの読み込みが完了しました')
+
+# 警告メッセージ（黄色）
+st.warning('ファイルサイズが大きい為、処理に時間がかかる可能性があります。')
+
+# エラーメッセージ（赤色）
+st.error('ファイルの形式が正しくありません。CSVファイルをアップロードしてください')
+# 成功メッセージ（緑色）
+st.success('グラフの作成が完了しました。')
+
+code = '''def hello():
+    print("Hello, Sreamlit")'''
+st.code(code, language='Python')
